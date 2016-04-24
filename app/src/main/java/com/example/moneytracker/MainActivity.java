733bb69c6616ad5ed67ce11a7889b75c76aa0acb
Pavel Arkhipov <pashko00710@yandex.ru>
2016-04-24 08:30:07 +0300

    Removed unnecessary code

--- app/src/main/java/com/example/moneytracker/MainActivity.java
+++ app/src/main/java/com/example/moneytracker/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.moneytracker;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -14,13 +10,9 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -28,14 +20,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
-    private FloatingActionButton expenseFabBtn;
-    private CoordinatorLayout rootLayout;
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
-//    private String[] myDataset;
-    ArrayList<MyListCosts> myDataset = new ArrayList<MyListCosts>();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +28,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         setupActionBar();
         setupDrawerLayout();
-        //setupRecyclerView();
-//        initInstances();
         if(savedInstanceState == null) {
             replaceFragment(new ExpenseFragment());
         }
@@ -59,25 +41,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                     updateTitleAndDrawer (f);
                 }
 
-            }
-        });
-    }
-
-    private void initInstances(View v) {
-        rootLayout = (CoordinatorLayout) findViewById(R.id.expense_fragment_coordinatorlayout);
-        expenseFabBtn = (FloatingActionButton) findViewById(R.id.expense_fabBtn);
-        expenseFabBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
-                Snackbar.make(rootLayout, "Hello. I am Snackbar!", Snackbar.LENGTH_SHORT)
-                        .setAction("Undo", new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-
-                            }
-                        })
-                        .show();
             }
         });
     }
@@ -119,24 +82,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             ft.commit();
         }
     }
-
-//    private void setupRecyclerView() {
-//        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-//
-//        // use this setting to improve performance if you know that changes
-//        // in content do not change the layout size of the RecyclerView
-//        mRecyclerView.setHasFixedSize(true);
-//
-//        // use a linear layout manager
-//        mLayoutManager = new LinearLayoutManager(this);
-//        mRecyclerView.setLayoutManager(mLayoutManager);
-//
-//        fillData();
-//
-//        // specify an adapter (see also next example)
-//        mAdapter = new MyAdapter(myDataset);
-//        mRecyclerView.setAdapter(mAdapter);
-//    }
 
     private void setupActionBar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -198,13 +143,5 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
         return true;
     }
-
-    // генерируем данные для адаптера
-//    public void fillData() {
-//        for (int i = 1; i <= 10; i++) {
-//            myDataset.add(new MyListCosts("Product " + i, i * 1000));
-//            myDataset.add(new MyListCosts("Cinema " + i, i * 1000));
-//        }
-//    }
 
 }
