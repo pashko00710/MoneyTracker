e1b2a5935c1d25c51141bc76cf6634376440c1ca
Pavel Arkhipov <pashko00710@yandex.ru>
2016-05-18 09:52:15 +0300

    Repeat all the steps webinar

--- app/src/main/java/com/example/moneytracker/rest/model/UserRegistrationModel.java
+++ app/src/main/java/com/example/moneytracker/rest/model/UserRegistrationModel.java
@@ -14,7 +14,7 @@ public class UserRegistrationModel {
     public String getStatus() {
         return status;
     }
-
+//
     public void setStatus(String status) {
         this.status = status;
     }
