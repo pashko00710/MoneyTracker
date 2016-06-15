c2017267c87d2cafc7d8cd4ffdf803d372cfac6b
Pavel Arkhipov <pashko00710@yandex.ru>
2016-06-15 22:07:56 +0300

    Add adapters

--- app/src/main/java/com/example/moneytracker/adapter/CategoriesAdapter.java
+++ app/src/main/java/com/example/moneytracker/adapter/CategoriesAdapter.java
@@ -9,31 +9,27 @@ import android.widget.TextView;
 import com.example.moneytracker.R;
 import com.example.moneytracker.database.model.Categories;
 
+import org.androidannotations.annotations.ViewById;
+
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 
-public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.CategoriesHolder> {
+public class CategoriesAdapter extends SelectableAdapter<CategoriesAdapter.CategoriesHolder> {
     private List<Categories> categoryList;
+    private ClickListener clickListener;
 
-    class CategoriesHolder extends RecyclerView.ViewHolder {
-        public TextView name;
-        public CategoriesHolder(View v) {
-            super(v);
-            name = (TextView) v.findViewById(R.id.category_name);
-        }
-
-
-    }
-
-    public CategoriesAdapter(List<Categories> categoryList) {
+    public CategoriesAdapter(List<Categories> categoryList, ClickListener clickListener) {
         this.categoryList = categoryList;
+        this.clickListener = clickListener;
     }
 
     @Override
     public CategoriesHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(
                 parent.getContext()).inflate(R.layout.category_item, parent, false);
-        CategoriesHolder vh = new CategoriesHolder(itemView);
+        CategoriesHolder vh = new CategoriesHolder(itemView, clickListener);
         return vh;
     }
 
@@ -41,11 +37,72 @@ public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.Ca
     public void onBindViewHolder(CategoriesHolder holder, int position) {
         Categories category = categoryList.get(position);
         holder.name.setText(category.getName());
+        holder.view.setVisibility(isSelected(position) ? View.VISIBLE : View.INVISIBLE);
     }
-
     @Override
     public int getItemCount() {
         return this.categoryList.size();
     }
+    public void refresh(List<Categories> data) {
+        categoryList.clear();
+        categoryList.addAll(data);
+        notifyDataSetChanged();
+    }
 
+    public void removeItems(List<Integer> positions) {
+        Collections.sort(positions, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer lhs, Integer rhs) {
+                return rhs - lhs;
+            }
+        });
+        while (!positions.isEmpty()) {
+            removeItem(positions.get(0));
+            positions.remove(0);
+        }
+    }
+    public void removeItem(int position) {
+        if (categoryList.get(position) != null) {
+            categoryList.get(position).delete();
+            categoryList.remove(position);
+            notifyItemRemoved(position);
+        }
+    }
+    private void removeCategory(int position) {
+        if (categoryList.get(position) != null) {
+            categoryList.get(position).delete();
+            categoryList.remove(position);
+        }
+    }
+    class CategoriesHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
+        @ViewById(R.id.selected_overlay)
+        View view;
+        public TextView name;
+        private ClickListener clickListener;
+        public CategoriesHolder(View v,  ClickListener clickListener) {
+            super(v);
+            name = (TextView) v.findViewById(R.id.category_name);
+            this.clickListener = clickListener;
+            v.setOnClickListener(this);
+            v.setOnLongClickListener(this);
+        }
+        public void bindCategory(Categories category) {
+            name.setText(category.getName());
+        }
+        @Override
+        public void onClick(View v) {
+            if (clickListener!= null){
+                clickListener.onItemClick(getAdapterPosition());
+            }
+        }
+        @Override
+        public boolean onLongClick(View v) {
+            if (clickListener != null) {
+                clickListener.onItemLongClick(getAdapterPosition());
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 }
